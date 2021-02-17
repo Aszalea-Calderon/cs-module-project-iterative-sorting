b89bfd9cbec524c6ab20114d6ba2499e838e0fd9
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-16 18:01:31 -0800

    solution

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,13 +1,15 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
+    for cur_index in range(0, len(arr) - 1):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
         # TO-DO: swap
         # Your code here
@@ -18,7 +20,12 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                print("BEFORE DAT SWITCH:",arr)
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                print("AFTER DAT SWITCH:",arr)
 
     return arr
 
@@ -39,8 +46,42 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+# def counting_sort(arr, maximum=None):
+#     # Your code here https://www.cs.usfca.edu/~galles/visualization/CountingSort.html, https://visualgo.net/bn/sorting
+#     if len(arr) == 0: # If arr is none, nah. Bye
+#         return
+#     max_pointer = int(max(arr)) #this will check for the max number in the arr
+#     if min(arr) < 0: # This checks if it any number is a negative
+#         print("Nah, why you putting in negitives. That's so mean.")
+#     # Buckets, Output and count
+#     output_bucket = [0] * len(arr) # This makes a list with 0's, this initializes it with however many zeros there are for the length of array. If you didn't put a number it would make multiple lists
+#     count_bucket = [0 for _ in range(max_pointer +1)] # this does the same as above. But PYTHON *sssssss*
+#     for element in arr:
+#         count_bucket[element] +=1
+#     for i in range(1,len(count_bucket)): # odd
+#         count_bucket[i] += count_bucket[i - 1]
+#     for i in range(len(arr) -1, -1, -1): # range takes 3 arguments, start, stop, and by how much,
+#         output_bucket[count_bucket[arr[i]-1 ]] = arr[i]
+#         count_bucket[arr[i]] -= 1
+#     for i in range(len(arr)):
+#         arr[i] = output_bucket[i]
+#     return arr
+
+
 def counting_sort(arr, maximum=None):
-    # Your code here
+    if arr == []:
+        return arr
+    if min(arr) < 0:
+        return "Error, negative numbers not allowed in Count Sort"
+    res = []
+    temp = [0 for _ in range(0, max(arr) + 1)]
+    for i in arr:
+        temp[i] += 1
+    for i in range(0,len(temp)):
+        while temp[i] > 0:
+            res.append(i)
+            temp[i] -= 1
+    return res
 
 
-    return arr
+
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,31 @@
 def linear_search(arr, target):
     # Your code here
-
+    # if target in arr:
+    #     return arr.index(target)
+    for index, element in enumerate(arr):  #O(n) -- Big O notation
+        if element == target:  # O(1)
+             return index
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+def binary_search(arr, target):  # O(log n) if emplimented correctly.
+
 
     # Your code here
+    index_left = 0
+    index_right = len(arr) -1  # length of arr -1 this be 0
 
+    while index_left <= index_right: # O(n) What happens inside the loop
+        middle = (index_left + index_right) // 2 # this is like .floor()
+        if arr[middle] > target:  # this cuts the itteration in half as it keeps skipping things.
+            index_right = middle -1   # O(1) time complexity. basic assignment doesn't kill it.
+        elif arr[middle] < target: # cuts in half again
+            index_left = middle +1 # O(1) time complexity. basic assignment doesn't kill it.
+        else: # stop itteration. Best case. # O(1) huzzah!
+            return middle  # It did it!
 
-    return -1  # not found
+    return -1  # not found, Nah it doesn't exist.
+
+# Big O is checking if the worst case happens how long does it take.
